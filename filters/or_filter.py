b9ef8a2106b4b2e5b38c322198ee62e254489cbb
Astler <vladyclaw@gmail.com>
2024-08-23 14:51:05 +0300

    Fixes, gachi memes

--- filters/or_filter.py
+++ filters/or_filter.py
@@ -6,6 +6,9 @@ from data.config import BOT_NAMES
 
 class RandomItemFilter(BaseFilter):
     async def __call__(self, message: types.Message) -> bool:
+        if message is None or message.text is None:
+            return False
+
         possible_names = (name.lower() for name in BOT_NAMES)
         message_text = message.text.lower()
         return any(message_text.startswith(name) for name in possible_names) and message_text.__contains__(" или ")
--- handlers/private/private_id.py
+++ handlers/private/private_id.py
@@ -1,13 +1,26 @@
+import random
+
 from aiogram import types, Router
 from aiogram.filters import Command
+from aiogram.types import Sticker
 
 from filters import IsPrivate
 from utils.misc import rate_limit
 
 id_router = Router()
 
+GOOD_SETS = ['video_gachi', 'ukrgachimemes']
 
 @rate_limit()
 @id_router.message(Command("id"), IsPrivate())
 async def bot_help(message: types.Message):
     await message.answer(str(message.from_user.id))
+
+
+@id_router.message(lambda message: message.sticker is not None)
+async def handle_sticker(message: types.Message):
+    sticker: Sticker = message.sticker
+    if sticker.set_name in GOOD_SETS:
+        sticker_set = await message.bot.get_sticker_set(sticker.set_name)
+        random_sticker = random.choice(sticker_set.stickers)
+        await message.answer_sticker(random_sticker.file_id)
