209902389ee27b06c4970eafca66b78a4e2a356e
Astler <vladyclaw@gmail.com>
2024-08-23 15:02:10 +0300

    Nikita

--- handlers/private/private_id.py
+++ handlers/private/private_id.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 from aiogram import types, Router
 from aiogram.filters import Command
@@ -10,6 +11,8 @@ from utils.misc import rate_limit
 id_router = Router()
 
 GOOD_SETS = ['video_gachi', 'ukrgachimemes']
+NIKITA = 'CAACAgIAAxkBAAIBH2bIegP8XT4sLEBH-PQYnvcUs39_AAJcUwACw5J5SCUKm-EflgGzNQQ'
+NIKITA_PATTERN = re.compile(r'\b(никит|nickyt|никитос|никитк|никитушк)\w*', re.IGNORECASE)
 
 @rate_limit()
 @id_router.message(Command("id"), IsPrivate())
@@ -24,3 +27,8 @@ async def handle_sticker(message: types.Message):
         sticker_set = await message.bot.get_sticker_set(sticker.set_name)
         random_sticker = random.choice(sticker_set.stickers)
         await message.answer_sticker(random_sticker.file_id)
+
+@id_router.message(lambda message: message.text is not None)
+async def handle_nikita_mention(message: types.Message):
+    if NIKITA_PATTERN.search(message.text):
+        await message.answer_sticker(NIKITA)
